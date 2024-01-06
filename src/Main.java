90d0dbb7b8b1f861d0b0fce90adfc5090eac3feb
shammy0923 <roseshammy@gmail.com>
2024-01-05 21:46:57 -0500

    Add prompts and user class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        User user = new User();
+
         // Basic Plan
+
+        System.out.println("Hello, what is your name?");
+        user.Name = scanner.next();
+
+        System.out.println("Hello " + user.Name + "," + " what is your budget goal?");
+        user.Goal = scanner.nextInt();
+
+        System.out.println("Enter target amount months for goal: ");
+        user.Planned_Months = scanner.nextInt();
+
+        System.out.println(user.Name + "," + " you'd like to save $" + user.Goal + " in " + user.Planned_Months + " months.");
 
         // Ask the user how much money they made this week
         // Ask the user to add expenses || Name of expense and cost || Keep repeating until users done
---
+++ src/User.java
@@ -0,0 +1,5 @@
+public class User {
+    public  String Name;
+    public int Goal;
+    public int Planned_Months;
+}
