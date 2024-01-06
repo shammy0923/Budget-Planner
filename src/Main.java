56c806ea9a3b0cc24db2acfa5dc92064f36bfe0f
shammy0923 <roseshammy@gmail.com>
2024-01-05 21:58:27 -0500

    Add projected monthly/weekly calculations

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         User user = new User();
 
-        // Basic Plan
+        // Basic Prompts
 
         System.out.println("Hello, what is your name?");
         user.Name = scanner.next();
@@ -18,7 +18,14 @@ public class Main {
         System.out.println("Enter target amount months for goal: ");
         user.Planned_Months = scanner.nextInt();
 
-        System.out.println(user.Name + "," + " you'd like to save $" + user.Goal + " in " + user.Planned_Months + " months.");
+        System.out.println("How much do you currently have saved?");
+        user.Current_Money = scanner.nextInt();
+
+        int Projected_Monthly_Amount = (user.Goal - user.Current_Money) / user.Planned_Months;
+        int Projected_Weekly_Amount = (Projected_Monthly_Amount / 4);
+
+        System.out.println("You will need to save $" + Projected_Monthly_Amount + " a month or $" + Projected_Weekly_Amount + " a week.");
+
 
         // Ask the user how much money they made this week
         // Ask the user to add expenses || Name of expense and cost || Keep repeating until users done
--- src/User.java
+++ src/User.java
@@ -2,4 +2,8 @@ public class User {
     public  String Name;
     public int Goal;
     public int Planned_Months;
+
+    public int Current_Money;
 }
+
+
